83caa98ebb88fb0655892d3787e57f112585432d
lkace <lkacenas@gmail.com>
2022-10-15 16:27:30 +0300

    Added Driver and TestListener classes

---
+++ src/test/java/lt/laurynas/utils/Driver.java
@@ -0,0 +1,34 @@
+package lt.laurynas.utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Driver { private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+
+    public static void setDriver() {
+        WebDriverManager.chromedriver().setup();
+
+//        driver = new ChromeDriver(setUpOptions());
+        driver.set(new ChromeDriver(setUpOptions()));
+        driver.get().manage().deleteAllCookies();
+//        driver.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    public static WebDriver getDriver() {
+        return driver.get();
+    }
+
+    public static void close() {
+        driver.get().quit();
+    }
+
+    private static ChromeOptions setUpOptions() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("window-size=2000,3000");
+        options.addArguments("--force-device-scale-factor=0.75");
+//        options.addArguments("headless");
+        return options;
+    }
+}
