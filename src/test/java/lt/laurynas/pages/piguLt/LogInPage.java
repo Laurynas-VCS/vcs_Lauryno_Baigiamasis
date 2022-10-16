169f186a24ab5491b0c26e26f6b72d96f5c72bc1
lkace <lkacenas@gmail.com>
2022-10-16 07:42:52 +0300

    Added LogInPage, LogInTest classes

---
+++ src/test/java/lt/laurynas/pages/piguLt/LogInPage.java
@@ -0,0 +1,62 @@
+package lt.laurynas.pages.piguLt;
+
+import lt.laurynas.pages.Common;
+import lt.laurynas.pages.Locators;
+
+public class LogInPage {
+
+    public static void open(String url) {
+        Common.openUrl(url);
+    }
+
+    public static void clickPrisijungtiIcon() {
+        Common.clickElement(Locators.Pigu.LogIn.clickPrisijungtiIcon);
+
+    }
+
+
+    public static void inputEmailAddress(String actualEmailAddress) {
+        Common.sendKeysToElement(
+                actualEmailAddress,
+                Locators.Pigu.LogIn.inputEmailAdress
+        );
+    }
+
+    public static void inputPassword(String actualPassword) {
+        Common.sendKeysToElement(
+                actualPassword,
+                Locators.Pigu.LogIn.inputPassword
+        );
+    }
+
+    public static void clickPrisijungti() {
+        Common.clickElement(Locators.Pigu.LogIn.clickPrisijungti);
+    }
+
+    public static void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String readMessage() {
+        return Common.getElementText(Locators.Pigu.LogIn.readMessageText);
+    }
+
+    public static String checkElementText() {
+        return Common.getElementText(Locators.Pigu.LogIn.checkElementText);
+    }
+
+    public static void clickProfileIcon() {
+        Common.clickElementByAction(Locators.Pigu.LogIn.clickProfileItem);
+    }
+
+    public static void clickAtsijungti() {
+        Common.clickElementByAction(Locators.Pigu.LogIn.clickAtsijungti);
+    }
+    public static String checkText(){
+        return Common.getElementText(Locators.Pigu.LogIn.checkText);
+    }
+}
---
+++ src/test/java/lt/laurynas/tests/piguLt/LogInTest.java
@@ -0,0 +1,74 @@
+package lt.laurynas.tests.piguLt;
+
+import lt.laurynas.pages.piguLt.LogInPage;
+import lt.laurynas.tests.TestBase;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class LogInTest extends TestBase {
+
+    @BeforeMethod
+    @Override
+    public void setUp() {
+        super.setUp();
+        LogInPage.open("https://pigu.lt/lt/");
+        LogInPage.clickPrisijungtiIcon();
+    }
+
+    @Test
+    private void logInWithValidInput() {
+
+        String actualEmailAddress = "lkacenas@gmail.com";
+        String actualPassword = "Lietuva21";
+        String expectedText = "Laurynas Kačėnas";
+        String actualText = "";
+
+        LogInPage.inputEmailAddress(actualEmailAddress);
+        LogInPage.inputPassword(actualPassword);
+        LogInPage.clickPrisijungti();
+        actualText = LogInPage.checkElementText();
+
+
+        Assert.assertEquals(actualText, expectedText);
+
+
+    }
+
+    @Test
+    private void logInWithInvalidInput() {
+
+        String actualEmailAddress = "vardenis@gmail.com";
+        String actualPassword = "Lietuva21";
+        String expectedMessage = "Neteisingi prisijungimo duomenys";
+        String actualMessage = "";
+
+        LogInPage.inputEmailAddress(actualEmailAddress);
+        LogInPage.inputPassword(actualPassword);
+        LogInPage.clickPrisijungti();
+        actualMessage = LogInPage.readMessage();
+
+        Assert.assertEquals(actualMessage, expectedMessage);
+
+    }
+    @Test
+    private void logout(){
+        String actualEmailAddress = "lkacenas@gmail.com";
+        String actualPassword = "Lietuva21";
+        String expectedText = "Prisijungti";
+        String actualText = "";
+
+        LogInPage.inputEmailAddress(actualEmailAddress);
+        LogInPage.inputPassword(actualPassword);
+        LogInPage.clickPrisijungti();
+        LogInPage.clickProfileIcon();
+        LogInPage.sleep(5000);
+        LogInPage.clickAtsijungti();
+        LogInPage.sleep(5000);
+       // actualText = LogInPage.checkText();
+
+     //  Assert.assertEquals(actualText, expectedText);
+
+    }
+}
+
